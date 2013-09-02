45ef240d5bfb42b5fbe332b340f84cc8ce087895
greatscotts <javelonagian@live.com>
2013-09-03 04:22:07 +0800

    Initial commit

---
+++ application/views/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	
+</head>
+<body>
+
+<div id="container">
+	<h1>FB CONNECT</h1>
+
+
+	<a href='<?php echo base_url().'main/facebook_request'; ?>'>Connect with facebook</a>
+
+</div>
+
+</body>
+</html>
---
+++ application/views/members.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Members only</title>
+
+	
+</head>
+<body>
+
+<div id="container">
+	<h1>Fwelcome</h1>
+
+
+
+<p><?php echo "hello $name"; ?></p>
+
+<p><?php echo "your email add is $email"; ?></p>
+	<a href='<?php echo base_url().'main/logout'; ?>'>logout</a>
+
+</div>
+
+</body>
+</html>
